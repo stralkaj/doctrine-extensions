e15254e6bf309dcbdacd15b65c7f6d547f5d8909
Jan Stralka <jsem@janstralka.cz>
2019-06-10 11:44:53 +0200

    BaseControl dao

--- src/Controls/BaseControl.php
+++ src/Controls/BaseControl.php
@@ -5,11 +5,18 @@ namespace OnlineImperium\Controls;
 /**
  * Base Control
  */
+
 use OnlineImperium\TemplateExtensions\TemplateFilters;
 use Nette\Application\UI\Control;
 
 class BaseControl extends Control
 {
+    /**
+     * @inject
+     * @var \App\Model\DaoManager
+     */
+    public $dao;
+
     protected function createTemplate()
     {
         $template = parent::createTemplate();
